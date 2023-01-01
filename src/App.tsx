28181fcc69e8739b0918d916ff6c147b00618f40
Oliver Nygren <oliver.nygren@icloud.com>
2023-01-01 22:09:10 +0100

    clean up and add imgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const HomePage = () => {
+	return (
+		<div className="root">
+			<div className="hero">
+				<img src="/images/logo-dark.svg" alt="Logo" className="header-logo" />
+				<div className="content">
+					<h1>Crafting websites to perfection</h1>
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et,
+						maiores at id cupiditate rem suscipit magnam ipsum ea rerum
+						accusamus sit optio cumque quia.
+					</p>
+					{/* <button className="cta-button"></button> */}
+				</div>
+				<img
+					src="/images/hero-hex.png"
+					alt="illustration"
+					className="hero-illustration"
+				/>
+			</div>
+		</div>
+	);
+};
 
-export default App;
+export default HomePage;
