b323377ab075c66b55b73ea365e83b6d4420d861
Oliver Nygren <oliver.nygren@icloud.com>
2023-01-02 12:50:40 +0100

    fetch data from hygraph

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
+import { gql, useQuery } from 'urql';
+
 import './App.css';
 
+const HeroQuery = gql`
+	query {
+		hero(where: { id: "clceolvo38tdd0cw6tk9fjfid" }) {
+			heading
+			logo {
+				id
+				url
+			}
+			textBody
+			id
+			heroIllustration {
+				id
+				url
+			}
+		}
+	}
+`;
+
 const HomePage = () => {
+	const [result] = useQuery({ query: HeroQuery });
+	const { data, fetching, error } = result;
+
+	if (fetching) return <p>Loading...</p>;
+	if (error)
+		return (
+			<p>
+				Error {error.message}
+				{error.stack}
+			</p>
+		);
+
 	return (
 		<div className="root">
 			<div className="hero">
-				<img src="/images/logo-dark.svg" alt="Logo" className="header-logo" />
+				<img src={data.hero.logo.url} alt="Logo" className="header-logo" />
 				<div className="content">
-					<h1>Crafting websites to perfection</h1>
+					<h1>{data.hero.heading}</h1>
+					{/* <h1>Crafting websites to perfection</h1> */}
 					<p>
 						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et,
 						maiores at id cupiditate rem suscipit magnam ipsum ea rerum
@@ -16,7 +49,7 @@ const HomePage = () => {
 					{/* <button className="cta-button"></button> */}
 				</div>
 				<img
-					src="/images/hero-hex.png"
+					src={data.hero.heroIllustration.url}
 					alt="illustration"
 					className="hero-illustration"
 				/>
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createClient, Provider } from 'urql';
+
 import './index.css';
 import HomePage from './App';
+
+const client = createClient({
+	url: 'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clceo6dam4v6601ule3a5dhud/master',
+});
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<HomePage />
+		<Provider value={client}>
+			<HomePage />
+		</Provider>
 	</React.StrictMode>
 );
